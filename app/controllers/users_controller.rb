be22c029c89240e63ba3451b43cbc8a9e7ab700e
Arturo Puente <arturo@able.co>
2019-04-08 16:33:38 -0500

    Add Users#create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,23 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.create(params[:user])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :first_name,
+      :last_name,
+      :username,
+      :email,
+      :email_confirmation,
+      :password,
+      :password_confirmation,
+      :password_hint
+    )
+  end
 end
